4d2a41684764892ad282653a2a0d4acc9e7e387f
Vitaliy <vitaliy.martunyuk@gmail.com>
2023-03-07 13:12:50 +0200

    Added an internet connection listener

--- app/src/main/java/com/example/income_app/MainActivity.java
+++ app/src/main/java/com/example/income_app/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.income_app;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.income_app.Fragments.MainFragment;
 import com.example.income_app.databinding.ActivityMainBinding;
+
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
         fragment_transition.replace(R.id.fragment_container_view,new MainFragment());
         fragment_transition.commit();
 
+        if(!isNetworkAvailable(this)){
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.no_internet)
+                    .setMessage(R.string.no_internet_desc)
+                    .setPositiveButton(android.R.string.yes, (dialog, which) -> finish())
+                    .setIcon(R.drawable.wifi_off)
+                    .setCancelable(false)
+                    .show();
+        }
+
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 
     @Override
